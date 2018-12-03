ff623b9e00e331571d7a9cf1546bf3030c631c97
muzzamil75 <muzamil.ijaz11@gmail.com>
2018-12-03 18:07:29 +0500

    Updates

---
+++ precache-manifest.d8aa2f4626c19ebd08e4344ebd9702a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8fea7a18d4d8522e729",
+    "url": "/Muzzamil75/React-update-state-code/static/js/runtime~main.b8fea7a1.js"
+  },
+  {
+    "revision": "ed5d8c4953c02a05403f",
+    "url": "/Muzzamil75/React-update-state-code/static/js/main.ed5d8c49.chunk.js"
+  },
+  {
+    "revision": "897d7ad89ba858aea485",
+    "url": "/Muzzamil75/React-update-state-code/static/js/1.897d7ad8.chunk.js"
+  },
+  {
+    "revision": "897d7ad89ba858aea485",
+    "url": "/Muzzamil75/React-update-state-code/static/css/1.acb1ecbd.chunk.css"
+  },
+  {
+    "revision": "d3dda0bf7891d91d6591a73b97587d24",
+    "url": "/Muzzamil75/React-update-state-code/index.html"
+  }
+];
