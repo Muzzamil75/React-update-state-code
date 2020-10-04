80a2d05651d9b830299caa9a0cfcb0e3fb01d541
muzzamil75 <muzamil.ijaz11@gmail.com>
2020-10-04 18:03:04 +0500

    ok

---
+++ build/precache-manifest.d536975defe0244edd65901c796bbc49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b31e3cb583d87bb06f80",
+    "url": "/Muzzamil75/Robo-Friends/static/js/runtime~main.b31e3cb5.js"
+  },
+  {
+    "revision": "b4217bca4b979d4df9af",
+    "url": "/Muzzamil75/Robo-Friends/static/js/main.b4217bca.chunk.js"
+  },
+  {
+    "revision": "c8a661c41a82b4cc35af",
+    "url": "/Muzzamil75/Robo-Friends/static/js/1.c8a661c4.chunk.js"
+  },
+  {
+    "revision": "c8a661c41a82b4cc35af",
+    "url": "/Muzzamil75/Robo-Friends/static/css/1.b7ff9f49.chunk.css"
+  },
+  {
+    "revision": "c3962071efa4b1d258be897c9144f5a8",
+    "url": "/Muzzamil75/Robo-Friends/index.html"
+  }
+];
